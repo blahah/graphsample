c2b079ac92e6d6ef36e666176256d58ab6b50627
Richard Smith-Unna <richardsmith404@gmail.com>
2015-05-27 00:56:00 +0100

    Add command-line options for input and output file

--- Makefile
+++ Makefile
@@ -2,7 +2,7 @@ LIBKHMER_DIR = "./third-party/khmer/lib"
 
 all: libkhmer
 	mkdir -p bin
-	g++ -std=c++11 -o bin/graphsample graphsample.cpp -I ./third-party/khmer/lib -L ./third-party/khmer/lib/ -lkhmer
+	g++ -std=c++11 -o bin/graphsample graphsample.cpp -I $(LIBKHMER_DIR) $(LIBKHMER_DIR)/libkhmer.a -I $(TCLAP_DIR)/include
 
 libkhmer:
 	$(MAKE) -C $(LIBKHMER_DIR)
--- graphsample.cpp
+++ graphsample.cpp
@@ -1,16 +1,15 @@
-// load a pair of fastq files into a compressible graph format
-// partition the graph
-// take a random subsample of the partitions
-// align reads to the sampled partitions and keep those that align
-
 #include <string>
 #include <vector>
 #include <random>
+#include <algorithm>
+#include <tclap/CmdLine.h>
 
 #include "sample.cpp"
 #include "hashbits.hh"
 
+using namespace TCLAP;
 using namespace khmer;
+
 
 Hashbits* new_hashbits() {
 
@@ -30,21 +29,47 @@ Hashbits* new_hashbits() {
 
 int main (int argc, char* argv[]) {
 
-  Hashbits *htable = new_hashbits();
+  try {
 
-  // load reads into the table
-  std::string read_pairs_in("interleaved.fq");
-  unsigned long long n_consumed = 0;
-  unsigned int total_reads = 0;
-  htable->consume_fasta_and_tag(read_pairs_in, total_reads, n_consumed);
+    // parse arguments
 
-  // partition
-  Part *subset_p = NULL;
-  subset_p = new Part(htable);
-  subset_p->do_partition(0, 0, false, true);
+    CmdLine cmd("graphsample - representative FASTQ subsampling",
+                ' ', "0.0.1");
 
-  // subsample partitions and output reads
-  std::string read_pairs_out("partitioned.fq");
-  subset_p->output_sampled_partitions(read_pairs_in, read_pairs_out);
+    ValueArg<std::string> inputArg("i", "input", "Read file in interleaved FASTQ format", true, "", "string");
+    cmd.add(inputArg);
+
+    ValueArg<std::string> outputArg("o", "output", "Path to write subsampled reads to", true, "", "string");
+    cmd.add(outputArg);
+
+    cmd.parse(argc, argv);
+
+    std::string input = inputArg.getValue();
+    std::string output = outputArg.getValue();
+
+    // run program
+
+    Hashbits *htable = new_hashbits();
+
+    // load reads into the table
+    std::string read_pairs_in(input);
+    unsigned long long n_consumed = 0;
+    unsigned int total_reads = 0;
+    htable->consume_fasta_and_tag(read_pairs_in, total_reads, n_consumed);
+
+    // partition
+    Part *subset_p = NULL;
+    subset_p = new Part(htable);
+    subset_p->do_partition(0, 0, false, true);
+
+    // subsample partitions and output reads
+    std::string read_pairs_out(output);
+    subset_p->output_sampled_partitions(read_pairs_in, read_pairs_out);
+
+
+  } catch (ArgException &e) {
+    std::cerr << "error: " << e.error() << " for arg " <<
+      e.argId() << std::endl;
+  }
 
 }
